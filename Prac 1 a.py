72ad5ea313d50d09cd0695420829a000b6e77f20
JAY RATHOD <54229728+JayRathod56@users.noreply.github.com>
2023-12-12 00:21:48 +0530

    Add files via upload

---
+++ Prac 1 a.py
@@ -0,0 +1,12 @@
+def operators():
+    print('Jay Rathod T-127')
+    val1=int(input('Enter value of val1: '))
+    val2=int(input('Enter value of val2: '))
+    print("Bitwise AND operator on val1 and val2 :",val1 & val2)
+    print("Bitwise OR operator on val1 and val2 :",val1 | val2)
+    print("Bitwise Exclusive OR operator on val1 and val2 :",val1 ^ val2)
+    print("Bitwise NOT operator on val1 :",~val1)
+    print("Bitwise LEFT SHIFT operator on val1 :",val1 << 1)
+    print("Bitwise RIGHT SHIFT operator on val2 :",val2 >> 1)
+
+operators()
